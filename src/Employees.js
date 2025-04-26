f9d58d994852205d282f4ac600edf42dd430c258
anuragvishwak <87541383+anuragvishwak@users.noreply.github.com>
2025-04-26 16:36:21 +0530

    made changed in navbar

--- src/Employees.js
+++ src/Employees.js
@@ -8,8 +8,8 @@ function Employees() {
 
   return (
     <div>
-      <div className="flex items-center justify-between sm:block">
-        <p className="sm:hidden text-xl p-3 text-[#2f4858] font-bold">Employees</p>
+      <div className="flex p-3 sm:p-0 items-center bg-[#2f4858] sm:bg-white justify-between sm:block">
+        <p className="sm:hidden text-xl text-white font-bold">Employees</p>
         <Navbar />
       </div>
       <div className="sm:p-3">
@@ -21,7 +21,7 @@ function Employees() {
             onClick={() => {
               setOpeningEmployeeForm(true);
             }}
-            className="bg-[#2f4858] mx-3 sm:mx-0 py-1.5 px-4 text-white"
+            className="bg-[#2f4858] text-sm mt-3 sm:mt-0 sm:text-base mx-3 sm:mx-0 py-1.5 px-4 text-white"
           >
             <div className="flex items-center space-x-2">
               <FaPlus />
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,77 +1,142 @@
 import React, { useState } from "react";
 import { IoMdNotifications } from "react-icons/io";
 import Notification from "./Notification";
-import { useNavigate } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import { FaBars, FaUser } from "react-icons/fa";
 
 function Navbar() {
   const navigate = useNavigate();
-  const [currentTab, setcurrentTab] = useState("Dashboard");
+  const location = useLocation();
   const [openingNotification, setOpeningNotification] = useState(false);
-
-
-console.log(currentTab)
+  const [openingNavbar, setopeningNavbar] = useState(false);
 
   return (
-    <div className="flex items-center justify-between m-3">
-     
-      <div className="hidden sm:flex items-center justify-center space-x-10 text-lg font-semibold ">
+    <div className="flex items-center justify-between sm:m-3">
+      <div className="hidden sm:flex items-center justify-center space-x-10 text-lg font-semibold">
         <button
-          onClick={() => {
-            setcurrentTab("Dashboard");
-            navigate("/");
-          }}
-          className={currentTab === "Dashboard" ? "text-[#9ee493]" : "text-[#2f4858]"}
+          onClick={() => navigate("/")}
+          className={
+            location.pathname === "/" ? "text-[#9ee493]" : "text-[#2f4858]"
+          }
         >
           Dashboard
         </button>
         <button
-          onClick={() => {
-            setcurrentTab("Employees");
-            navigate("/Employees");
-          }}
-          className={currentTab === "Employees" ? "text-[#9ee493]" : "text-[#2f4858]"}
+          onClick={() => navigate("/Employees")}
+          className={
+            location.pathname === "/Employees"
+              ? "text-[#9ee493]"
+              : "text-[#2f4858]"
+          }
         >
           Employees
         </button>
         <button
-          onClick={() => {
-            setcurrentTab("Projects");
-            navigate("/Projects");
-          }}
-          className={currentTab === "Projects" ? "text-[#9ee493]" : "text-[#2f4858]"}
+          onClick={() => navigate("/Projects")}
+          className={
+            location.pathname === "/Projects"
+              ? "text-[#9ee493]"
+              : "text-[#2f4858]"
+          }
         >
           Projects
         </button>
         <button
-          onClick={() => {
-            setcurrentTab("Clients");
-            navigate("/Clients");
-          }}
-          className={currentTab === "Clients" ? "text-[#9ee493]" : "text-[#2f4858]"}
+          onClick={() => navigate("/Clients")}
+          className={
+            location.pathname === "/Clients"
+              ? "text-[#9ee493]"
+              : "text-[#2f4858]"
+          }
         >
           Clients
         </button>
         <button
-          onClick={() => {
-            setcurrentTab("Billing & Payments");
-            navigate("/Billing & Payments");
-          }}
+          onClick={() => navigate("/BilllingPayments")}
           className={
-            currentTab === "Billing & Payments" ? "text-[#9ee493]" : "text-[#2f4858]"
+            location.pathname === "/BilllingPayments"
+              ? "text-[#9ee493]"
+              : "text-[#2f4858]"
           }
         >
           Billing & Payments
         </button>
       </div>
+
       <div className="flex items-center justify-center space-x-2">
-        <button className="bg-[#2f4858] text-white p-2 rounded-full" onClick={() => setOpeningNotification(!openingNotification)}>
-            <IoMdNotifications size={22} />
+        <button
+          onClick={() => {
+            setopeningNavbar(!openingNavbar);
+          }}
+          className="sm:bg-[#2f4858] sm:hidden text-white sm:p-2.5 rounded-full"
+        >
+          <FaBars />
         </button>
-        <button className="bg-[#2f4858] text-white p-2.5 rounded-full">
-          <FaUser/>
+        <button
+          className="sm:bg-[#2f4858] text-white sm:p-2 rounded-full"
+          onClick={() => setOpeningNotification(!openingNotification)}
+        >
+          <IoMdNotifications size={22} />
+        </button>
+        <button className="sm:bg-[#2f4858] text-white sm:p-2.5 rounded-full">
+          <FaUser />
         </button>
       </div>
+
+      {openingNavbar && (
+        <div className="fixed top-10 right-10 z-50 sm:hidden">
+          <div className="flex flex-col p-4 shadow-xl bg-white h-auto w-full">
+          <button
+            onClick={() => navigate("/")}
+            className={
+              location.pathname === "/" ? "text-[#9ee493]" : "text-[#2f4858]"
+            }
+          >
+            Dashboard
+          </button>
+          <button
+            onClick={() => navigate("/Employees")}
+            className={
+              location.pathname === "/Employees"
+                ? "text-[#9ee493]"
+                : "text-[#2f4858]"
+            }
+          >
+            Employees
+          </button>
+          <button
+            onClick={() => navigate("/Projects")}
+            className={
+              location.pathname === "/Projects"
+                ? "text-[#9ee493]"
+                : "text-[#2f4858]"
+            }
+          >
+            Projects
+          </button>
+          <button
+            onClick={() => navigate("/Clients")}
+            className={
+              location.pathname === "/Clients"
+                ? "text-[#9ee493]"
+                : "text-[#2f4858]"
+            }
+          >
+            Clients
+          </button>
+          <button
+            onClick={() => navigate("/BilllingPayments")}
+            className={
+              location.pathname === "/BilllingPayments"
+                ? "text-[#9ee493]"
+                : "text-[#2f4858]"
+            }
+          >
+            Billing & Payments
+          </button>
+        </div>
+          </div>
+      )}
 
       {openingNotification && (
         <Notification setOpeningNotification={setOpeningNotification} />
